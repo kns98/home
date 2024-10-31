73c86de075e3586c5dc4d3e32cd7be990a69c2b4
Kevin Sheth <Kevin Sheth@LAPTOP-59VI7GUR>
2024-10-31 10:10:11 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+House house = new House();
+Console.WriteLine(house.ToString());
+
+Console.ReadLine();
+
+
+class House
+{
+    int NumRooms = 3;
+    bool hasKitchen = true;
+    bool hasBuiltinKitchen = false;
+
+    public override string ToString()
+    {
+        string KitchenOut = "";
+        if (hasKitchen)
+        {
+            KitchenOut = " with kitchen ";
+
+            if (hasBuiltinKitchen)
+            {
+                KitchenOut += " attached to dining room";
+            }
+        }
+
+        return "NumRooms: " + NumRooms + KitchenOut;
+    }
+}
+
+
